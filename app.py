cf6706fe98ed9da158fe5f46e60126d808a321b5
Jacob <Jacobp1997@live.co.uk>
2022-09-25 19:44:22 +0100

    master

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import yfinance as yf
 st.title('Test')
 
 tickers = ('TSLA', 'AAPL', 'MSFT', 'BTC-USD', 'ETH-USD',
-           'XDC-USD', 'CUDOS-USD', 'OMI-USD', 'LINK-USD', 'QNT-USD', 'FTSE 100')
+           'XDC-USD', 'CUDOS-USD', 'OMI-USD', 'LINK-USD', 'QNT-USD', '^FTSE')
 
 dropdown = st.multiselect('Pick your assets', tickers)
 
