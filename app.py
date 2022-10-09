d8e7b76ee3850680cd7b8576e224122fbb078316
Jacob <jacobp1997@live.co.uk>
2022-10-09 14:19:42 +0100

    master

--- app.py
+++ app.py
@@ -73,15 +73,15 @@ uploaded_file = st.sidebar.file_uploader(
     label="Upload your CSV or Excel file", type=['csv', 'xlsx'])
 
 
-global df
+global df1
 if uploaded_file is not None:
     print(uploaded_file)
     print("hello")
     try:
-        df = pd.read_csv(uploaded_file)
+        df1 = pd.read_csv(uploaded_file)
     except Exception as e:
         print(e)
-        df = pd.read_excel(uploaded_file)
+        df1 = pd.read_excel(uploaded_file)
 
 
 def relativeret(df):
